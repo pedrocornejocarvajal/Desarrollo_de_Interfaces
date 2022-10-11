bb3e167c2e1f7ed2b629cd5e0165820c795119e2
pedrocornejocarvajal <75272975+pedrocornejocarvajal@users.noreply.github.com>
2022-10-11 14:26:17 +0200

    11/10/2022

---
+++ 06-EjercicioU6/06-EjercicioU6_MAUI/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace _06_EjercicioU6_MAUI;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ 06-EjercicioU6/06-EjercicioU6_MAUI/obj/Debug/net6.0-android/android/src/crc6484a9a702c6e51fb4/MainActivity.java
@@ -0,0 +1,46 @@
+package crc6484a9a702c6e51fb4;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("_06_EjercicioU6_MAUI.MainActivity, 06-EjercicioU6_MAUI", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("_06_EjercicioU6_MAUI.MainActivity, 06-EjercicioU6_MAUI", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("_06_EjercicioU6_MAUI.MainActivity, 06-EjercicioU6_MAUI", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
