c1b75b19855db844497f8a2cb855cd7bfc440d4d
pedrocornejocarvajal <75272975+pedrocornejocarvajal@users.noreply.github.com>
2022-10-28 15:35:50 +0200

    commit 2

---
+++ 08-Ejercicio5/Entidades/clsPersona.cs
@@ -0,0 +1,27 @@
+﻿namespace Entidades
+{
+    public class clsPersona
+    {
+
+        #region
+
+        private String nombre { get; set; }
+        private String apellidos { get; set; }
+        private String dni { get; set; }
+
+        #endregion
+
+        #region constructores
+
+        public clsPersona() { }
+
+        public clsPersona( String nombre, String apellidos, String dni)
+        {
+            this.dni = dni;
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+
+        }
+        #endregion
+    }
+}
