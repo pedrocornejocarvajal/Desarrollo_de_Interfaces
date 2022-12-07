c1dea21f1e69894d449c59a32ec120d2e7da38e3
pedrocornejocarvajal <75272975+pedrocornejocarvajal@users.noreply.github.com>
2022-12-07 14:40:24 +0100

    dzfgfxdg

--- CRUD_Personas/CRUD_Personas/CRUD_Personas_Maui/obj/Debug/net6.0-ios/iossimulator-x64/CRUD_Personas_Maui.AssemblyInfo.cs
+++ CRUD_Personas/CRUD_Personas/CRUD_Personas_Maui/obj/Debug/net6.0-ios/iossimulator-x64/CRUD_Personas_Maui.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyProductAttribute("CRUD_Personas_Maui")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUD_Personas_Maui")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
-[assembly: System.Runtime.Versioning.TargetPlatformAttribute("iOS15.4")]
+[assembly: System.Runtime.Versioning.TargetPlatformAttribute("iOS16.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("iOS14.2")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
